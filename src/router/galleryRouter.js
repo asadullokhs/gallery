4317683e1e817405fb6e28d86fd7e42cf697d74c
Asadullokh <uzbdevit@gmail.com>
2024-01-20 06:33:13 +0500

    added download

--- src/router/galleryRouter.js
+++ src/router/galleryRouter.js
@@ -6,6 +6,7 @@ const galleryCtrl = require("../controller/galleryController");
 router.post("/", galleryCtrl.addPhoto);
 router.get("/", galleryCtrl.getPhotos);
 router.get("/search", galleryCtrl.searchPhoto);
+router.get("/download/:id", galleryCtrl.download);
 router.delete("/:id", galleryCtrl.deletePhoto);
 router.put("/:id", galleryCtrl.updatePhoto);
 
